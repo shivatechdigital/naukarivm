cfce38393a655e058a4173f73342611a4e7c6fe9
Prashant Rao <prashant.rao-ext+hpeprod@hpe.com>
2026-09-11 02:49:47 +0530

    Fix profile auth module dependencies

--- backend/src/profiles/profiles.module.ts
+++ backend/src/profiles/profiles.module.ts
@@ -3,8 +3,10 @@ import { Module } from '@nestjs/common';
 import { ProfilesController } from './profiles.controller';
 import { ProfilesService } from './profiles.service';
 import { PrismaService } from '../prisma.service';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [ProfilesController],
   providers: [
     ProfilesService,
